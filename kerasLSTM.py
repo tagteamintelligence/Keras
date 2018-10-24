2fc5b784c08a964846a07495e9881810c1d02998
tagteamintelligence <44304509+tagteamintelligence@users.noreply.github.com>
2018-10-24 12:48:10 -0700

    20 period

--- kerasLSTM.py
+++ kerasLSTM.py
@@ -20,7 +20,7 @@ from numpy import newaxis
 import pandas as pd
 from matplotlib import pyplot as plt
 
-n=100
+n=20
 def load_data(n):
 	df = pd.read_csv("Data/EUR_Keras.csv")
 	chunked_df = [df[i:i+n].values.tolist() for i in range(0,df.shape[0],n)]
@@ -37,22 +37,22 @@ def result_data(n):
 	return np.array(result[1:])
 
 x_train = load_data(n)
-y_train = result_data(n).reshape(49)
+y_train = result_data(n).reshape(249)
 
 print(x_train.shape, y_train.shape)
 scaler = MinMaxScaler(feature_range=(0,1))
-x_train = scaler.fit_transform((x_train).reshape(-1,117600))
-x_train = x_train.reshape(49,100,24)
+x_train = scaler.fit_transform((x_train).reshape(-1,119520))
+x_train = x_train.reshape(249,20,24)
 
 model = Sequential()
-model.add(LSTM(input_shape=(100,24), return_sequences=True, units=50))
+model.add(LSTM(input_shape=(20,24), return_sequences=True, units=50))
 model.add(Dropout(0.2))
 model.add(LSTM(100, return_sequences=False))
 model.add(Activation('linear'))
 model.add(Dense(1, activation='linear'))
 
 model.compile(loss='mse', optimizer='rmsprop',)
-history = model.fit(x_train, y_train, batch_size=100, epochs=50)
+history = model.fit(x_train, y_train, batch_size=10, epochs=20)
 
 #history = model.fit(X, Y, epochs=100, validation_data=(valX, valY))
 plt.plot(history.history['loss'])
@@ -62,7 +62,7 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper right')
 plt.show()
-
+'''
 def load_test_data():
 	df = pd.read_csv("C:/Users/Kurt/Desktop/Python_CSV_Data/CSVData/testrun.csv")
 	x = df.values.tolist()
@@ -73,3 +73,4 @@ def load_test_data():
 #plot_results_multiple(predict,0.1,99)
 prediction = model.predict(load_test_data())
 print(prediction)
+'''
