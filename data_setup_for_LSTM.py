77c9f6759df720826defa59c6710c425581d6cb1
tagteamintelligence <44304509+tagteamintelligence@users.noreply.github.com>
2018-10-26 09:42:26 -0700

    Add files via upload

--- data_setup_for_LSTM.py
+++ data_setup_for_LSTM.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np 
 
 config = configparser.ConfigParser()
-config.read('C:/Users/Kurt/Desktop/pythonCode/Python_CSV_Data/config/config_v20.ini')
+config.read('C:/Users/Kurt/Desktop/pythonCode/oanda_2018/config/config_v20.ini')
 accountID = config['oanda']['account_id']
 access_token = config['oanda']['api_key']
 client = oandapyV20.API(access_token=access_token)
@@ -15,22 +15,28 @@ def Run(instrument, params):
 	candleData = instruments.InstrumentsCandles(instrument=instrument, params=params)
 	candleData = client.request(candleData)
 	candleData = candleData ['candles']
-	#print(candleData)
-	df = pd.DataFrame()
-	for i in candleData:
-		df = pd.concat([df,pd.Series([[float(i['mid']['o']),float(i['mid']['h']),float(i['mid']['l']),float(i['mid']['c'])]])])
-		df = df.reset_index(drop=True)
-	return df
+	df = pd.DataFrame([[str(x['mid']['o']) for x in candleData],
+		[str(x['mid']['h']) for x in candleData],
+	    [str(x['mid']['l']) for x in candleData],
+	    [str(x['mid']['c']) for x in candleData],
+	    [str(x['volume']) for x in candleData]]).T
+	df.columns = ['open','high','low','close','volume']
+	return df[['open','high','low','close']]
 
 def GetData(instrument, candleCount, granularity):
 	params = {
 	    "count": candleCount,
 	    "granularity": granularity
 	}
-
 	bigFrame = pd.DataFrame()
 	for x in range(len(instrument)):
 	    bigFrame = pd.concat([bigFrame, Run(instrument[x], params)], axis=1)
-	bigFrame = bigFrame.T
-	bigFrame.index = instrument
 	return bigFrame
+
+def GetResultData(instrument, candleCount, granularity):
+	params = {
+	    "count": candleCount,
+	    "granularity": granularity
+	}
+	df = Run(instrument, params)[['close']]
+	return df
--- kerasLSTM.py
+++ kerasLSTM.py
@@ -1,55 +1,53 @@
-import tensorflow as tf
 import keras 
-
-from keras import backend as K
+# Model
 from keras.models import Sequential
-from keras.layers import Activation
 from keras.layers.core import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.optimizers import Adam
-from keras.metrics import categorical_crossentropy
-
-import time
-from random import randint
+# Support
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.utils import shuffle
-
 import numpy as np
-from numpy import newaxis
-import pandas as pd
 from matplotlib import pyplot as plt
 from data_setup_for_LSTM import GetData
+from data_setup_for_LSTM import GetResultData
 
 ### USER INPUT ###
+main_pair = "EUR_USD"
 instrument = ["EUR_AUD","EUR_CAD","EUR_CHF","EUR_GBP","EUR_NZD","EUR_USD"]
-candleCount = 20
 granularity = 'H1'
+batch_size = 24
+nCycle = 200
+epochs = 20
+candleCount = batch_size*nCycle
 
-df = GetData(instrument, candleCount, granularity)
-df_array = np.array(df.values.tolist())
-x_train = df_array
-y_train = df_array
+# Array Data
+x_train = np.array(GetData(instrument, candleCount, granularity).values)
+y_train = np.array(GetResultData(main_pair, candleCount, granularity).values)
+
+# Batch Sizing
+x_train = x_train[:-batch_size]
+y_train = y_train[batch_size:]
+
+# Reshapeing
+x_train = x_train.reshape(x_train.shape[0],1,x_train.shape[1])
+x_train_shape = x_train.shape
+
+# Scale
+scaler = MinMaxScaler(feature_range=(0,1))
+x_train = scaler.fit_transform((x_train).reshape(-1,x_train.shape[1]))
+x_train = x_train.reshape(x_train_shape)
 
 print(x_train.shape, y_train.shape)
-scaler = MinMaxScaler(feature_range=(0,1))
-x_train = scaler.fit_transform((x_train).reshape(-1,119520))
-x_train = x_train.reshape(249,20,24)
-
-exit()
-
+# create LSTM
 model = Sequential()
-model.add(LSTM(input_shape=(20,24), return_sequences=True, units=50))
+model.add(LSTM(units= 100, input_shape=(x_train_shape[1], x_train_shape[2])))
 model.add(Dropout(0.2))
-model.add(LSTM(100, return_sequences=False))
-model.add(Activation('linear'))
-model.add(Dense(1, activation='linear'))
+model.add(Dense(units=1, activation='linear'))
+model.compile(loss='mean_squared_error', optimizer='adam')
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2)
+model.save('Models/EUR_24period_LSTM.h5')
 
-model.compile(loss='mse', optimizer='rmsprop')
-model.fit(x_train, y_train, batch_size=10, epochs=20)
-model.save('Models/EUR_20period_LSTM.h5')
-
-history = model.fit(x_train, y_train, batch_size=10, epochs=20)
 plt.plot(history.history['loss'])
 plt.title('train loss')
 plt.ylabel('loss')
