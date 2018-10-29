c27295f50ea1a00f45693c150cc627e515a195fe
tagteamintelligence <44304509+tagteamintelligence@users.noreply.github.com>
2018-10-29 12:16:27 -0700

    3D Time Series Data

--- LSTM_data.py
+++ LSTM_data.py
@@ -3,14 +3,19 @@ import oandapyV20
 import oandapyV20.endpoints.instruments as instruments
 import oandapyV20.endpoints.orders as orders
 import pandas as pd
+import numpy as np
 
 config = configparser.ConfigParser()
-config.read('C:/Users/Kurt/Desktop/pythonCode/oanda_2018/config/config_v20.ini')
+config.read('../../oanda_2018/config/config_v20.ini')
 accountID = config['oanda']['account_id']
 access_token = config['oanda']['api_key']
 client = oandapyV20.API(access_token=access_token)
 
-def Run(instrument, params):
+def Data(instrument, candleCount, granularity, shift, time_series, close_only=False):
+	params = {
+	    "count": candleCount,
+	    "granularity": granularity
+	}
 	candleData = instruments.InstrumentsCandles(instrument=instrument, params=params)
 	candleData = client.request(candleData)
 	candleData = candleData ['candles']
@@ -20,22 +25,37 @@ def Run(instrument, params):
 	    [str(x['mid']['c']) for x in candleData],
 	    [str(x['volume']) for x in candleData]]).T
 	df.columns = ['open','high','low','close','volume']
-	return df[['open','high','low','close']]
+	out = pd.DataFrame([df['high'][shift:shift+time_series],
+						df['low'][shift:shift+time_series],
+						df['close'][shift:shift+time_series]])
+	if close_only == True:
+		out = pd.DataFrame([df['close'][shift:shift+1]])
+		out = np.array(out.values)
+		out = np.transpose(out)
+		out = out.reshape(1,1,1)
+		return out
+	out = np.array(out.values)
+	out = np.transpose(out)
+	out = out.reshape(1,time_series,3)
+	return out
 
-def GetData(instrument, candleCount, granularity):
-	params = {
-	    "count": candleCount,
-	    "granularity": granularity
-	}
-	bigFrame = pd.DataFrame()
-	for x in range(len(instrument)):
-	    bigFrame = pd.concat([bigFrame, Run(instrument[x], params)], axis=1)
-	return bigFrame
-
-def GetResultData(instrument, candleCount, granularity):
-	params = {
-	    "count": candleCount,
-	    "granularity": granularity
-	}
-	df = Run(instrument, params)[['close']]
-	return df
+def RunData(instrument, candleCount, granularity, time_series, close_only=False):
+	if close_only == True:
+		big_main_frame = np.empty((0,1,len(instrument)*1))
+		main_frame = np.empty((1,1,0))
+	else:
+		big_main_frame = np.empty((0,time_series,len(instrument)*3))
+		main_frame = np.empty((1,time_series,0))
+	shift = 0
+	for x in range(candleCount-time_series):
+		for i in range(len(instrument)):
+			data = Data(instrument[i], candleCount, granularity, shift, time_series, close_only)
+			main_frame = np.append(main_frame, data, 2)
+		shift += 1
+		big_main_frame = np.append(big_main_frame, main_frame, 0)
+		if close_only == True:
+			main_frame = np.empty((1,1,0))
+		else:
+			main_frame = np.empty((1,time_series,0))
+		print('Data: '+str(shift)+'/'+str(candleCount-time_series))
+	return big_main_frame
--- LSTM_model.py
+++ LSTM_model.py
@@ -9,28 +9,25 @@ from keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from matplotlib import pyplot as plt
-from LSTM_data import GetData
-from LSTM_data import GetResultData
+from LSTM_data import RunData
 
 ### USER INPUT ###
-main_pair = "EUR_USD"
+main_pair = ["EUR_USD"]
 instrument = ["EUR_AUD","EUR_CAD","EUR_CHF","EUR_GBP","EUR_NZD","EUR_USD"]
 granularity = 'H1'
-batch_size = 24
+time_series = 24
 nCycle = 200
 epochs = 20
-candleCount = batch_size*nCycle
+candleCount = time_series*nCycle
+print('CandleCount:',candleCount,'of MAX 5000')
 
 # Array Data
-x_train = np.array(GetData(instrument, candleCount, granularity).values)
-y_train = np.array(GetResultData(main_pair, candleCount, granularity).values)
+x_train = RunData(instrument, candleCount, granularity, time_series)
+y_train = RunData(main_pair, candleCount, granularity, time_series, close_only=True).reshape(candleCount-time_series ,1)
 
 # Batch Sizing
-x_train = x_train[:-batch_size]
-y_train = y_train[batch_size:]
-
-# Reshapeing
-x_train = x_train.reshape(x_train.shape[0],1,x_train.shape[1])
+x_train = x_train[:-1]
+y_train = y_train[1:]
 x_train_shape = x_train.shape
 
 # Scale
@@ -38,15 +35,15 @@ scaler = MinMaxScaler(feature_range=(0,1))
 x_train = scaler.fit_transform((x_train).reshape(-1,x_train.shape[1]))
 x_train = x_train.reshape(x_train_shape)
 
-print(x_train.shape, y_train.shape)
-# create LSTM
+# Create LSTM Model
 model = Sequential()
-model.add(LSTM(units= 100, input_shape=(x_train_shape[1], x_train_shape[2])))
+model.add(LSTM(100, return_sequences=True, input_shape=(x_train_shape[1], x_train_shape[2])))
 model.add(Dropout(0.2))
-model.add(Dense(units=1, activation='linear'))
+model.add(LSTM(units= 100, return_sequences=False))
+model.add(Dense(activation="linear", units=1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2)
-model.save('Models/EUR_24period_LSTM.h5')
+history = model.fit(x_train, y_train, batch_size=1, epochs=epochs, verbose=1)
+#model.save('Models/'+main_pair+'_'+'time_series'+'_LSTM_test.h5')
 
 plt.plot(history.history['loss'])
 plt.title('train loss')
