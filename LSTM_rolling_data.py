2ea3c5515ce9f74ed0b956b02588a006ff9d2ee8
tagteamintelligence <44304509+tagteamintelligence@users.noreply.github.com>
2018-11-05 15:30:13 -0800

    Add files via upload

--- LSTM_rolling_data.py
+++ LSTM_rolling_data.py
@@ -8,17 +8,23 @@ candleCount = 1000
 df = Data(instrument, candleCount, granularity)
 df = pd.DataFrame(df)
 df.columns = ['high','low','close']
-rolling_list = [10,20,30,40,50,60]
+rolling_list = [i for i in range(5,50,5)]
 
 def labeler(df, window=10):
 	df['label_%d' % window] = 0
 	for i in range(len(df['close'])):
 		i_max = df['close'][window if (i-window) < window else i-window:i+window].max()
+		i_min = df['close'][window if (i-window) < window else i-window:i+window].min()
 		if float(df['close'][i]) == float(i_max):
+			df['label_%d' % window].loc[i] = 1
+		if float(df['close'][i]) == float(i_min):
 			df['label_%d' % window].loc[i] = 1
 	return df
 		
-for windows in rolling_list:
-	labeler(df, windows)
+def label_maker(rolling_list):
+	for windows in rolling_list:
+		labeler(df, windows)
 
+
+label_maker(rolling_list)
 print(df)
